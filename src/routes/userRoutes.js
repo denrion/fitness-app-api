601ec0609af79f99e41ed8795c0a48edf6ca3854
Nemanja Kuzmanovic <kuzmaneca@gmail.com>
2020-07-20 15:26:47 +0200

    disable auth

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -6,14 +6,11 @@ const {
   getUser,
   updateUser,
 } = require('../controllers/userController');
-const isAuth = require('../middleware/isAuth');
-const restrictTo = require('../middleware/restrictTo');
-const Role = require('../constants/Role');
 
 const router = express.Router();
 
 // Routes below will go through both isAuth & restrictTo middleware first
-router.use(isAuth, restrictTo(Role.ADMIN));
+// router.use(isAuth, restrictTo(Role.ADMIN));
 
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
