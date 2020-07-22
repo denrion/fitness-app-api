a782634499b82c1ea97376d1e1c5adccd06755bb
Nemanja Kuzmanovic <kuzmaneca@gmail.com>
2020-07-22 14:52:21 +0200

    add populate to GetTraining

--- src/controllers/trainingController.js
+++ src/controllers/trainingController.js
@@ -20,7 +20,7 @@ const getAllTrainings = getAll(Training);
  * @route     GET /api/v1/Trainings/:TrainingId
  * @access    Private
  */
-const getTraining = getOne(Training);
+const getTraining = getOne(Training, { path: 'exercises' });
 
 /**
  * @desc      Create New Training
